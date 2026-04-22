7479d04d382304b7cbac834a8e8bc93bd00a1d77
l <108607271+lsx314@users.noreply.github.com>
2026-04-22 23:27:02 +0800

    Add files via upload

---
+++ HGMAF_Stage2_AdaSeq/data/data_collators/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+from .multilabel_typing_data_collator_with_padding import (
+    MultiLabelSpanTypingDataCollatorWithPadding,
+)
+from .pretraining_data_collator_with_padding import PretrainingDataCollatorWithPadding
+from .sequence_labeling_data_collator_with_padding import (
+    SequenceLabelingDataCollatorWithPadding,
+)
+from .span_extraction_data_collator_with_padding import (
+    SpanExtractionDataCollatorWithPadding,
+)
+from .twostage_data_collator_with_padding import TwostageDataCollatorWithPadding
+from .word_extraction_data_collator_with_padding import (
+    WordExtractionDataCollatorWithPadding,
+)
