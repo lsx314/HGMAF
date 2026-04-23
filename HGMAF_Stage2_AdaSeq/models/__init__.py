20be5c65b89e34dc77ea6dd6be29ed388564a3ed
l <108607271+lsx314@users.noreply.github.com>
2026-04-23 11:57:07 +0800

    Create __init__.py

---
+++ HGMAF_Stage2_AdaSeq/models/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+
+from .biaffine_ner_model import BiaffineNerModel
+from .global_pointer_model import GlobalPointerModel
+from .multilabel_typing_model import (
+    MultiLabelConcatTypingModel,
+    MultiLabelConcatTypingModelMCCES,
+    MultiLabelSpanTypingModel,
+)
+from .pretraining_model import PretrainingModel
+from .relation_extraction_model import RelationExtractionModel
+from .sequence_labeling_model import SequenceLabelingModel
+from .hgmaf_model import HGMAFModel
+from .twostage_ner_model import TwoStageNERModel
+from .w2ner_model import W2NerModel
